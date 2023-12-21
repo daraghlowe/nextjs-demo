1fadb5e727b33bf904ef5be7470ef8419f833908
Daragh Lowe <daragh.lowe@wpengine.com>
2023-12-21 15:53:43 +0000

    update next.js config for cache handler

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const nextConfig = {
+    /* other next config options here */
+    experimental: atlas_cache_handler(),
+}
+
+function atlas_cache_handler() {
+    if (process.env.ATLAS_CACHE_HANDLER_ENABLED !== undefined) {
+        return {
+            incrementalCacheHandlerPath: require.resolve('./.atlas/atlas-cache-handler.js'),
+            isrMemoryCacheSize: 0
+        }
+    }
+
+    return undefined
+}
